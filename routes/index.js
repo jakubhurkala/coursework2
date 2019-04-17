0aac1fe154a21009dc10922c70c2ceca8c2057cd
0xMo <40332133@live.napier.ac.uk>
2019-04-17 14:25:20 +0200

    This file is for the routes between the pages that start by '/' route

---
+++ routes/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const mongoose = require('mongoose');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const config = require('../config/database');
+const passport = require('passport');
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+// Check for DB errors
+db.on('error', function(err){
+  console.log(err);
+});
+
+// Making sure db working
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+// Express Session Middleware
+router.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+router.use(require('connect-flash')());
+router.use(function(req, res, next) {
+  res.locals.messages = require("express-message")(req, res);
+  next();
+});
+
+// Express Validator Middleware
+router.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Encryption/Decryption Tools' });
+});
+
+router.get('/CaesarCipher', function(req, res, next) {
+  res.render('CaesarCipher', {
+    title: 'Caesar Cipher'
+  });
+});
+
+router.get('/MorseCode', function(req, res, next) {
+  res.render('MorseCode', {
+    title: 'Morse Code'
+  });
+});
+
+router.get('/BruteForce', function(req, res, next) {
+  res.render('BruteForce', {
+    title: 'Brute Force Attck with Caesar Cipher'
+  });
+});
+
+module.exports = router;
